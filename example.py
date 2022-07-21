f967cd5829fc527bb1a080143db3b6a40084ef82
Lr2k <Lr2k.Coding@gmail.com>
2022-07-21 22:01:56 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,13 +1,34 @@
+from email import message
 import ktnetscraper as kt
 
+import os
 import getpass
-id = input("StudentID: ")
-password = getpass.getpass("Password: ")
-scraper = kt.Scraper(id=id, password=password)
 
-scraper.calc_faculty()
-scraper.calc_grade()
 
+scraper = kt.Scraper()
+
+# 学籍番号・パスワードの入力とログイン処理
+while True:
+    id = input("StudentID: ")
+    password = getpass.getpass("Password: ")
+
+    try:
+        scraper.login(id=id, password=password)
+        break
+    except Exception as e:
+        # scraper.log はLoggerオブジェクトで、ログを保持する機能を持つ。
+        # logメソッドにより新規のログを追加。
+        scraper.log.log(status=1, message=e)
+        print("ログインに失敗しました。")
+
+# 入力された学籍番号から学部、学年を推測。
+try:
+    scraper.calc_faculty()
+    scraper.calc_grade()
+except Exception as e:
+    scraper.log.log(status=1, message=e)
+
+# 学年が正しいか確認
 require_grade = False
 while True:
     print("学年は" + scraper.grade + "ですか？ [Y/n]")
@@ -21,30 +42,30 @@ while True:
     else:
         pass
 
+# 学年が異なっていた場合に正しい学年を入力
 while require_grade:
     grade = int(input("学年："))
 
     if (1<=grade) and (grade<=6):
         scraper.grade = str(grade)
         require_grade = False
-try:
-    scraper.login()
-    scraper.log.store()
-except Exception as e:
-    scraper.log.log(status=1, message=e, show=True)
-    scraper.log.store()
-    exit()
 
+# 日付は文字列で指定
+# リストやタプルに複数格納して指定可能
 date = ["20220602", "20220603", "20220721"]
 
+# 時間割ページから教材のダウンロードページのURLを取得。
 try:
     dl_page_url, year_list = scraper.get_dlpage_url(date, return_year=True)
     scraper.log.store()
 except Exception as e:
     scraper.log.log(status=1, message=e, show=True)
+    # storeメソッドによりこれまでのログをファイルに出力する。
+    # デフォルトではカレントディレクトリに "scraper.log" という名前で保存される。
     scraper.log.store()
     exit()
 
+# 教材のダウンロードページから教材のダウンロードURLを含む情報を取得しTextオブジェクトに格納。
 try:
     unit = kt.data_structures.Unit(scraper.get_text_info(dl_page_url, year_list))
     scraper.log.store()
@@ -53,17 +74,26 @@ except Exception as e:
     scraper.log.store()
     exit()
 
-
+# 情報を取得した教材の一覧を表示
+# titlesで一覧を表示する位に並べる項目を指定
 titles = ["unit", "thema", "date", "period", "target", "file_name"]
-
 unit.show(titles=titles, separate_with_line=True)
 
-SAVE_DIR = "folder1"
-
-try:
-    scraper.dl(unit, save_dir=SAVE_DIR)
-    scraper.log.store()
-except Exception as e:
-    scraper.log.log(status=1, message=e, show=True)
-    scraper.log.store()
-    exit()
+# 教材をダウンロードし保存
+# ユニットごとにフォルダを分けて保存
+SAVE_DIR_BASE = "folder1"
+save_dir_list = os.listdir(SAVE_DIR_BASE)
+for text in unit.text_list:
+    try:
+        save_dir_path = os.path.join(SAVE_DIR_BASE, text.unit)
+        if text.unit not in save_dir_list:
+            os.makedirs(save_dir_path)
+            save_dir_list.append(text.unit)
+        
+        scraper.dl(text, save_dir_path)
+        scraper.log.store()
+    
+    except Exception as e:
+        scraper.log.log(status=1, message=e, show=True)
+        scraper.log.store()
+        exit()
